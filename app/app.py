a940bc81ab5496d7edfb7edc3b2baee0ac3e819f
qianzach <qianzach@gmail.com>
2021-11-15 23:15:45 -0800

    added data frame functionality
    merp

--- app/app.py
+++ app/app.py
@@ -18,90 +18,6 @@ def base():
 
 @app.route('/networks')
 def networks():
-#    df = pd.DataFrame({
-#       'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas'],
-#       'Amount': [4, 1, 2, 2, 4, 5],
-#       'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
-#    })
-#    fig = px.bar(df, x='Fruit', y='Amount', color='City',    barmode='group')
-   
-    # Create random graph
-    # G = nx.random_geometric_graph(200, 0.125)
-
-    # # Create Edges
-    # edge_x = []
-    # edge_y = []
-    # for edge in G.edges():
-    #     x0, y0 = G.nodes[edge[0]]['pos']
-    #     x1, y1 = G.nodes[edge[1]]['pos']
-    #     edge_x.append(x0)
-    #     edge_x.append(x1)
-    #     edge_x.append(None)
-    #     edge_y.append(y0)
-    #     edge_y.append(y1)
-    #     edge_y.append(None)
-
-    # edge_trace = go.Scatter(
-    #     x=edge_x, y=edge_y,
-    #     line=dict(width=0.5, color='#888'),
-    #     hoverinfo='none',
-    #     mode='lines')
-
-    # node_x = []
-    # node_y = []
-    # for node in G.nodes():
-    #     x, y = G.nodes[node]['pos']
-    #     node_x.append(x)
-    #     node_y.append(y)
-
-    # node_trace = go.Scatter(
-    #     x=node_x, y=node_y,
-    #     mode='markers',
-    #     hoverinfo='text',
-    #     marker=dict(
-    #         showscale=True,
-    #     # colorscale options
-    #     #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-    #     #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-    #     #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-    #         colorscale='YlGnBu',
-    #         reversescale=True,
-    #         color=[],
-    #         size=10,
-    #         colorbar=dict(
-    #             thickness=15,
-    #             title='Node Connections',
-    #             xanchor='left',
-    #          titleside='right'
-    #         ),
-    #         line_width=2))
-
-    # # Color Node Points
-    # node_adjacencies = []
-    # node_text = []
-    # for node, adjacencies in enumerate(G.adjacency()):
-    #     node_adjacencies.append(len(adjacencies[1]))
-    #     node_text.append('# of connections: '+str(len(adjacencies[1])))
-
-    # node_trace.marker.color = node_adjacencies
-    # node_trace.text = node_text
-
-    # # Create Graph
-    # fig = go.Figure(data=[edge_trace, node_trace],
-    #             layout=go.Layout(
-    #                 title='<br>Network graph made with Python',
-    #                 titlefont_size=16,
-    #                 showlegend=False,
-    #                 hovermode='closest',
-    #                 margin=dict(b=20,l=5,r=5,t=40),
-    #                 annotations=[ dict(
-    #                     text="Insert Text",
-    #                     showarrow=False,
-    #                     xref="paper", yref="paper",
-    #                     x=0.005, y=-0.002 ) ],
-    #                 xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-    #                 yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-    #                 )
     url = "../web_scraper/no_games.csv"
     df = pd.read_csv(url)
 
@@ -185,14 +101,17 @@ def networks():
                         x=0.005, y=-0.002) ],
                     xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
                     yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
+    fig.update_yaxes(automargin=True)
 
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     header = "Interactive Network Graph"
+    #export the csv
     return render_template('fig.html', header=header,graphJSON=graphJSON)
 
 @app.route('/recommendations')
 def recommendations():
+    #load csv and output
     df = pd.DataFrame({
         "Vegetables": ["Lettuce", "Cauliflower", "Carrots", "Lettuce", "Cauliflower", "Carrots"],
         "Amount": [10, 15, 8, 5, 14, 25],
