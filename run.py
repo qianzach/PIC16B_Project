64328f6e01f0a2014ff3ab427907a315744d0f21
renzotw <renzo.atw@gmail.com>
2021-11-16 16:30:45 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from dash_package import app
 
+# To run the app locally, locate the directory, activate conda environment, and finally type python run.py in terminal
+
 if __name__ == "__main__":
     app.run_server(debug=True)
